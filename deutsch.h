de974e864acb06670772f255f73efccbd99ea783
adverbialsatz <134710711+adverbialsatz@users.noreply.github.com>
2026-02-06 14:04:13 +0100

    Add files via upload

---
+++ deutsch.h
@@ -0,0 +1,36 @@
+﻿#ifndef DEUTSCH_H
+#define DEUTSCH_H
+
+#define ausbrechen break
+#define fall case
+#define weitermachen continue
+#define standard default
+#define tue do
+#define sonst else
+#define aufzählung enum
+#define für for
+#define gehezu goto
+#define falls if
+#define ausgeben return
+#define vorzeichenlos unsigned
+#define zeichen char
+#define konstant const
+#define doppel double
+#define gleitkomma float
+#define zahl int
+#define langezahl long
+#define kurzezahl short
+#define vorzeichen signed
+#define größevon sizeof
+#define statisch static
+#define struktur struct
+#define switch schalter
+#define union union
+#define void nichts
+#define volatile volatil
+#define while während
+#define wahr true
+#define falsch false
+#define druckf printf
+
+#endif // DEUTSCH_H
